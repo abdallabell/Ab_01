3fe314cf274e8cbfded82600e31c1950925e0c26
Abdallah <abdallahbeallaoueur2004@gmail.com>
2025-04-20 21:00:20 +0100

    Replace Base64 with RSA

--- App.java
+++ App.java
@@ -1,7 +1,17 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to SecureApp!");
-    }
+import java.security.*;
+import javax.crypto.Cipher;
 
-    
+public class App {
+    public static void main(String[] args) throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair pair = keyGen.generateKeyPair();
+
+        String text = "hello";
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        System.out.println("Encrypted (RSA): " + new String(encrypted));
+    }
 }
+
